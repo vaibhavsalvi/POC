efb718da4472e141dd48d1e72073e7e41d7ee424
vaibhavsalvi <vaibhav.v.salvi@gmail.com>
2018-05-06 23:58:23 +0530

    Distributed Cache .Net POC
    Distributed Cache POC for REDIS and IGNITE

---
+++ IGNITEPOC/ConsoleApp5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Cache;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ignite=Ignition.Start();        
+            ICache<Int32, String> cache = ignite.GetOrCreateCache<int, string>("myCache");
+
+            // Store keys in cache (values will end up on different cache nodes).
+            for (int i = 0; i < 10; i++)
+                cache.Put(0, "Hello");
+
+            for (int i = 0; i < 10; i++)
+               Console.WriteLine("Got [key=" + i + ", val=" + cache.Get(0) + ']');
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ REDISPOC/ConsoleApp4/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack;
+using ServiceStack.Redis;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = "localhost";
+
+            string key = "IDG";
+
+            // Store data in the cache
+
+            bool success = Save(host, key, "Hello World!");
+
+            // Retrieve data from the cache using the key
+
+            Console.WriteLine("Data retrieved from Redis Cache: " + Get(host, key));
+
+            Console.Read();
+        }
+        private static bool Save(string host, string key, string value)
+
+        {
+
+            bool isSuccess = false;
+
+            using (RedisClient redisClient = new RedisClient(host))
+
+            {
+
+                if (redisClient.Get<string>(key) == null)
+
+                {
+
+                    isSuccess = redisClient.Set(key, value);
+
+                }
+
+            }
+
+            return isSuccess;
+
+        }
+
+        private static string Get(string host, string key)
+
+        {
+
+            using (RedisClient redisClient = new RedisClient(host))
+
+            {
+
+                return redisClient.Get<string>(key);
+
+            }
+
+        }
+    }
+}
